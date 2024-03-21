1c2315f21c3f618207c351d527c7381226661558
Adambob12 <164278005+Adambob12@users.noreply.github.com>
2024-03-20 21:34:20 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Playing Boston","Watching Boston"];
 
 
 let currentIndex = 0;
